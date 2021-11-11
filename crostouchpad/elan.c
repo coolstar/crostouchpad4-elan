60edc81865594da63b0a6c45bb1f3e7cf90c264d
CoolStar <coolstarorganization@gmail.com>
2021-11-10 20:55:34 -0800

    Clean Up. Update for VS2019 + WDK10

--- crostouchpad/elan.c
+++ crostouchpad/elan.c
@@ -1,5 +1,5 @@
 #define DESCRIPTOR_DEF
-#include <driver.h>
+#include "driver.h"
 
 #define bool int
 
@@ -420,7 +420,7 @@ BOOLEAN OnInterruptIsr(
 	SpbReadDataSynchronously(&pDevice->I2CContext, 0, &touchpadReport, sizeof(touchpadReport));
 
 	if (touchpadReport[0] == 0xff) {
-		return;
+		return true;
 	}
 
 	struct _ELAN_MULTITOUCH_REPORT report;
@@ -708,28 +708,6 @@ IN PWDFDEVICE_INIT DeviceInit
 	}
 
 	//
-	// Because we are a virtual device the root enumerator would just put null values 
-	// in response to IRP_MN_QUERY_ID. Lets override that.
-	//
-
-	minorFunction = IRP_MN_QUERY_ID;
-
-	status = WdfDeviceInitAssignWdmIrpPreprocessCallback(
-		DeviceInit,
-		ElanEvtWdmPreprocessMnQueryId,
-		IRP_MJ_PNP,
-		&minorFunction,
-		1
-		);
-	if (!NT_SUCCESS(status))
-	{
-		ElanPrint(DEBUG_LEVEL_ERROR, DBG_PNP,
-			"WdfDeviceInitAssignWdmIrpPreprocessCallback failed Status 0x%x\n", status);
-
-		return status;
-	}
-
-	//
 	// Setup the device context
 	//
 
@@ -838,110 +816,6 @@ IN PWDFDEVICE_INIT DeviceInit
 
 	devContext->DeviceMode = DEVICE_MODE_MOUSE;
 	devContext->FxDevice = device;
-
-	return status;
-}
-
-NTSTATUS
-ElanEvtWdmPreprocessMnQueryId(
-WDFDEVICE Device,
-PIRP Irp
-)
-{
-	NTSTATUS            status;
-	PIO_STACK_LOCATION  IrpStack, previousSp;
-	PDEVICE_OBJECT      DeviceObject;
-	PWCHAR              buffer;
-
-	PAGED_CODE();
-
-	//
-	// Get a pointer to the current location in the Irp
-	//
-
-	IrpStack = IoGetCurrentIrpStackLocation(Irp);
-
-	//
-	// Get the device object
-	//
-	DeviceObject = WdfDeviceWdmGetDeviceObject(Device);
-
-
-	ElanPrint(DEBUG_LEVEL_VERBOSE, DBG_PNP,
-		"ElanEvtWdmPreprocessMnQueryId Entry\n");
-
-	//
-	// This check is required to filter out QUERY_IDs forwarded
-	// by the HIDCLASS for the parent FDO. These IDs are sent
-	// by PNP manager for the parent FDO if you root-enumerate this driver.
-	//
-	previousSp = ((PIO_STACK_LOCATION)((UCHAR *)(IrpStack)+
-		sizeof(IO_STACK_LOCATION)));
-
-	if (previousSp->DeviceObject == DeviceObject)
-	{
-		//
-		// Filtering out this basically prevents the Found New Hardware
-		// popup for the root-enumerated Elan on reboot.
-		//
-		status = Irp->IoStatus.Status;
-	}
-	else
-	{
-		switch (IrpStack->Parameters.QueryId.IdType)
-		{
-		case BusQueryDeviceID:
-		case BusQueryHardwareIDs:
-			//
-			// HIDClass is asking for child deviceid & hardwareids.
-			// Let us just make up some id for our child device.
-			//
-			buffer = (PWCHAR)ExAllocatePoolWithTag(
-				NonPagedPool,
-				ELAN_HARDWARE_IDS_LENGTH,
-				ELAN_POOL_TAG
-				);
-
-			if (buffer)
-			{
-				//
-				// Do the copy, store the buffer in the Irp
-				//
-				RtlCopyMemory(buffer,
-					ELAN_HARDWARE_IDS,
-					ELAN_HARDWARE_IDS_LENGTH
-					);
-
-				Irp->IoStatus.Information = (ULONG_PTR)buffer;
-				status = STATUS_SUCCESS;
-			}
-			else
-			{
-				//
-				//  No memory
-				//
-				status = STATUS_INSUFFICIENT_RESOURCES;
-			}
-
-			Irp->IoStatus.Status = status;
-			//
-			// We don't need to forward this to our bus. This query
-			// is for our child so we should complete it right here.
-			// fallthru.
-			//
-			IoCompleteRequest(Irp, IO_NO_INCREMENT);
-
-			break;
-
-		default:
-			status = Irp->IoStatus.Status;
-			IoCompleteRequest(Irp, IO_NO_INCREMENT);
-			break;
-		}
-	}
-
-	ElanPrint(DEBUG_LEVEL_VERBOSE, DBG_IOCTL,
-		"ElanEvtWdmPreprocessMnQueryId Exit = 0x%x\n", status);
 
 	return status;
 }
